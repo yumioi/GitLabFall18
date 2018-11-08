673c57ee28da9cd7a1d0d7b24cbd20e1ee7284f1
Brandon L <blanuevo1@toromail.csudh.edu>
2018-11-07 17:13:58 -0800

    added my part, fixed indent and errors

--- rpgGame.c
+++ rpgGame.c
@@ -1,4 +1,5 @@
 //Contributors
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
@@ -8,269 +9,292 @@
 
 void main(void)
 {
-	int x,y,z,i,h,g,k,choice=0;
-	char name[256];
-	int boxNum=0;
+    int x,y,z,i,h,g,k,choice=0;
+    char name[256];
+    int boxNum=0;
 
-	printf("Please enter your name: "); //Input any number of array inputs
-	scanf("%s",name);
+    printf("Please enter your name: "); //Input any number of array inputs
+    scanf("%s",name);
 
-	printf("Hello %s welcome to the rpgGame!\n",name);
-	
-	while(choice != 99)
-	{
-		puts("You find yourself in a dark room and you are not sure how you got here.");
-        	puts("As you look around you see the room has 25 doors, each labeled with a number. You are not sure how such a small room can have 25 doors, sooo magic...");
-        	puts("The room starts filling with water and you must choose a door to open or you will likely drown. you may quit anytime by selecting option 99");
-        	puts("What door do you choose?");
-        	scanf("%d",&choice);
-		
-		switch(choice)
-		{
-			case 1:
-			{
-				while(choice != 99)
-				{
+    printf("Hello %s welcome to the rpgGame!\n",name);
 
-				}
-				break;
-			}
-			case 2:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 3:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 4:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 5:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 6:
-			{
-					while(choice != 99)
-					{
-					}
-					break;
-			}
-			case 7:
-			{
-					while(choice != 99)
-				{ 
-				}
-					break;
-			}
-			case 8:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 9:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 10:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 11:
-			{
-					while(choice != 99)
-					{
-					}
-					break;
-			}
-			case 12:
-			{
-					while(choice != 99)
-					{
-					}
-					break;
-			}
-			case 13:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 14:
-			{
-					while(choice != 99)
-					{
-					}
-					break;
-			}
-			case 15:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 16:
-			{
-					while(choice != 99)
-					{
-					}
-					break;
-			}
-			case 17:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 18:			
-			{
-					while(choice != 99)
-					{
-					}
-					break;
-			}
-			case 19:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 20:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 21:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 22:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 23:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 24:
-			{
-					while(choice != 99)
-					{
-							puts("you open the door and find ........");
-							scanf("%d",&choice);
-					}
-					break;
-			}
-			case 25:
-			{
-					while(choice != 99)
-					{
-							puts("You open the door and close it behind you.");
-							puts("After you overcome the panic from almost drowning, you look around and You find yourself in a cave, the air is damp and you smell mold.");
-							puts("You notice a skeleton at your feet with it's right hand clenched around something. The cave ahead leads to a tunnel and you see a door to your right.");
-							puts("At this point you have 3 choices:");
-							puts("1. Examine the skeleton.");
-							puts("2. Proceed further ahead in the cave.");
-							puts("3. Enter the door to your right.");
-							scanf("%d",&choice);
-							if(choice == 1)
-							{
-								puts("You reach down and pry open the skeleton's hand, a finger breaks loose and you place it in your pocket. Once you pry the opject free you look at it closely in the light and see it is a live grenade and the pin springs free. You drop the grenade and dash through the cave. You can hear the grenade explode, collapsing the tunnel behind you.");
-								puts("To be continued...");
-								break;
-							}
-							else if(choice == 2)
-							{
-								puts("You find yourself further ahead in the cave.");
-								puts("To be continued....");
-								break;
-							}
-							else if(choice == 3)
-							{
-								puts("You enter the and close the door behind you.");
-								printf("You hear an loud voice \" %s why do you disturb me? \" \n",name);
-                                                                puts("To be continued....");
-								break;
-							}
-							else
-							{
-								puts("wrong choice");
-							}
-					}
-					break;
-				
-			
-			
-				
-			}
-		}	
-	}
+    while(choice != 99)
+    {
+        puts("You find yourself in a dark room and you are not sure how you got here.");
+        puts("As you look around you see the room has 25 doors, each labeled with a number. You are not sure how such a small room can have 25 doors, sooo magic...");
+        puts("The room starts filling with water and you must choose a door to open or you will likely drown. you may quit anytime by selecting option 99");
+        puts("What door do you choose?");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1:
+                {
+                    while(choice != 99)
+                    {
+                        puts("You open the door and you quickly turn arround to find the door quickly disappearing behind you.");
+                        puts("In front of you, you now notice a long hallway leading to a room that is completely white.");
+                        puts("At this point you have two choices.");
+                        puts("1. Walk down the dark hallway.");
+                        puts("2. Scream in panic.");
+                        scanf("%d", &choice);
+                        if(choice == 1)
+                        {
+                            puts("You begin to wall down the dark hallway.");
+                            puts("As you walk, you notice that the room is actually a lot brighter than you thought it was.");
+                            puts("You now are inside the white room and it looks like a office cubicle.");
+                            puts("In the far right corner you see a old computer..");
+                            puts("System Error.");
+                            puts("[675.362959] Kernel panic - not syncing: Watchdog detected hard LOCKUP on cpu 8");
+                            exit(0);
+                        }
+                        else if(choice == 2)
+                        {
+                            puts("You then scream at the top of your lungs!");
+                            puts("Surprisingly, nothing happens.");
+                            puts("----------------------");
+                            puts("Feelings of helplessness grow inside you.");
+                            puts("----------------------");
+                        }
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 4:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 5:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 6:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 7:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 8:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 9:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 10:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 11:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 12:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 13:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 14:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 15:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 16:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 17:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 18:
+                {
+                    while(choice != 99)
+                    {
+                    }
+                    break;
+                }
+            case 19:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 20:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 21:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 22:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 23:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 24:
+                {
+                    while(choice != 99)
+                    {
+                        puts("you open the door and find ........");
+                        scanf("%d",&choice);
+                    }
+                    break;
+                }
+            case 25:
+                {
+                    while(choice != 99)
+                    {
+                        puts("You open the door and close it behind you.");
+                        puts("After you overcome the panic from almost drowning, you look around and You find yourself in a cave, the air is damp and you smell mold.");
+                        puts("You notice a skeleton at your feet with it's right hand clenched around something. The cave ahead leads to a tunnel and you see a door to your right.");
+                        puts("At this point you have 3 choices:");
+                        puts("1. Examine the skeleton.");
+                        puts("2. Proceed further ahead in the cave.");
+                        puts("3. Enter the door to your right.");
+                        scanf("%d",&choice);
+                        if(choice == 1)
+                        {
+                            puts("You reach down and pry open the skeleton's hand, a finger breaks loose and you place it in your pocket. Once you pry the opject free you look at it closely in the light and see it is a live grenade and the pin springs free. You drop the grenade and dash through the cave. You can hear the grenade explode, collapsing the tunnel behind you.");
+                            puts("To be continued...");
+                            break;
+                        }
+                        else if(choice == 2)
+                        {
+                            puts("You find yourself further ahead in the cave.");
+                            puts("To be continued....");
+                            break;
+                        }
+                        else if(choice == 3)
+                        {
+                            puts("You enter the and close the door behind you.");
+                            printf("You hear an loud voice \" %s why do you disturb me? \" \n",name);
+                            puts("To be continued....");
+                            break;
+                        }
+                        else
+                        {
+                            puts("wrong choice");
+                        }
+                    }
+                    break;
+
+
+
+
+                }
+        }
     }
+}
 
